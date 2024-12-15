224db06498eede3e0fcc0dcf33b79909e9efb469
schnee98 <jooeun06161@gmail.com>
2024-12-15 16:37:58 +0900

    feat: 첫 페이지에 백그라운드 애니메이션 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { BackgroundAnimation } from "./_components/BackgroundAnimation";
+import FadeContent from "./_components/FadeContent";
+import styles from "./_components/headers/common.module.css";
+
 export default function Home() {
-  return <></>;
+  const [isFadingOut, setIsFadingOut] = useState(false);
+
+  const handleClick = () => {
+    setIsFadingOut(true);
+  };
+
+  return (
+    <>
+      <BackgroundAnimation />
+      <FadeContent index={0} isFadingOut={isFadingOut} delay={3}>
+        <h1 className={styles.logo}>schuterview</h1>
+        <h1 className={styles.headeer}>모 의 개 발 자 인 터 뷰</h1>
+      </FadeContent>
+      <FadeContent index={1} isFadingOut={isFadingOut} delay={3}>
+        <button className={styles.submit} onClick={handleClick}>
+          시작하기
+        </button>
+      </FadeContent>
+    </>
+  );
 }
